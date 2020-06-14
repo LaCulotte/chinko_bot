7d9b34f858d21e3a81525703f9bc91c49ef4c4d0
Christophe <negeko-inscriptions@hotmail.com>
2020-06-14 21:07:14 +0200

    Program to help Network Type creation

---
+++ helpers/NetworkTypeGenerator.py
@@ -0,0 +1,97 @@
+#! /usr/bin/python
+
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import font
+from tkinter import ttk
+
+className = input("type class name ?\n")
+print(className)
+
+parentName = input("Parent class (blank = NetworkType) ?\n")
+if(parentName == ""):
+    parentName = "NetworkType"
+print(parentName)
+
+type_id = input("Type id?\n")
+print(type_id)
+
+# with_serialize = input("With serialize/desirealize? (y/n) (blank = yes)\n")
+
+attributes = []
+is_continue = input("Attributes? (y/n) (blank = yes)\n")
+while(is_continue == 'y' or is_continue == 'Y' or is_continue == ""):
+    attributes.append(input("Type '<type> <attribute name> (blank = no more attributes)'\n"))
+    if(attributes[-1] == ""):
+        attributes.pop()
+        break
+    is_continue = input("Other attributes? (y/n) (blank = yes)\n")
+
+root = tk.Tk()
+root.option_add('*foreground', 'black')  # set all tk widgets' foreground to red
+root.option_add('*activeForeground', 'black')  # set all tk widgets' foreground to red
+root.withdraw()
+
+file_path = filedialog.askdirectory(master=root, initialdir = "..")
+
+f_h = open(file_path + "/" + className + ".h", 'w')
+
+f_h.write("#ifndef " + className.upper() + "_H\n")
+f_h.write("#define " + className.upper() + "_H\n")
+f_h.write("\n")
+f_h.write("#include \"" + parentName + ".h\"\n")
+f_h.write("\n")
+f_h.write("class " + className + " : public " + parentName + " {\n")
+f_h.write("public: \n")
+f_h.write("\t// Constructor\n")
+f_h.write("\t" + className + "() {};\n")
+f_h.write("\t// Copy constructor\n")
+f_h.write("\t" + className + "(const " + className + "& other) = default;\n")
+f_h.write("\n")
+f_h.write("\t// Copy operator\n")
+f_h.write("\t" + className + "& operator=(const " + className + "& other) = default;\n")
+f_h.write("\t// Destructor\n")
+f_h.write("\t~" + className + "() = default;\n")
+f_h.write("\n")
+f_h.write("// Returns the type's id")
+f_h.write("virtual unsigned int getId() { return typeId; };")
+f_h.write("// Type's id")
+f_h.write("static const unsigned int typeId = " + type_id + ";")
+
+# if(with_serialize != 'n' and with_serialize != 'N'):
+if(parentName != ""):
+    f_h.write("\t// Turns raw data into the usable data (type's attributes)\n")
+    f_h.write("\tvirtual bool deserialize(shared_ptr<MessageDataBuffer> input) override;\n")
+    f_h.write("\t// Turns the type's attributes into raw data\n")
+    f_h.write("\tvirtual bool serialize(shared_ptr<MessageDataBuffer> output) override;\n")
+    f_h.write("\n")
+else:
+    f_h.write("\t// Turns raw data into the usable data (type's attributes)\n")
+    f_h.write("\tvirtual bool deserialize(shared_ptr<MessageDataBuffer> input);\n")
+    f_h.write("\t// Turns the type's attributes into raw data\n")
+    f_h.write("\tvirtual bool serialize(shared_ptr<MessageDataBuffer> output);\n")
+    f_h.write("\n")
+for att in attributes:
+    f_h.write("\t" + att + ";\n")
+f_h.write("\n")
+f_h.write("};\n")
+f_h.write("\n")
+f_h.write("#endif")
+
+f_h.close()
+
+f_cpp = open(file_path + "/" + className + ".cpp", 'w')
+
+f_cpp.write("#include \"" + className +".h\"\n")
+f_cpp.write("\n")
+f_cpp.write("bool " + className + "::serialize(shared_ptr<MessageDataBuffer> output) {\n")
+f_cpp.write("\n")
+f_cpp.write("\treturn true;\n")
+f_cpp.write("}\n")
+f_cpp.write("\n")
+f_cpp.write("bool " + className + "::deserialize(shared_ptr<MessageDataBuffer> input) {\n")
+f_cpp.write("\n")
+f_cpp.write("\treturn true;\n")
+f_cpp.write("}\n")
+
+f_cpp.close()
